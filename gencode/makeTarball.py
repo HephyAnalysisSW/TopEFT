d88b776790784a169c551bcf23973379d73a6011
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2017-08-25 09:18:28 +0200

    remove "_" separation from parsing of couplings. Add some logging.
    Former-commit-id: 25a59c33104890618f01db55e868f0b58b4dbf7d

--- gencode/makeTarball.py
+++ gencode/makeTarball.py
@@ -6,46 +6,54 @@ from TopEFT.gencode.EFT import *
 import TopEFT.tools.logger as logger
 
 argParser = argparse.ArgumentParser(description = "Argument parser")
-argParser.add_argument('--model',       action='store', default='HEL_UFO',          nargs='?',   choices=['HEL_UFO', 'TopEffTh'], help="Which madgraph model?")
-argParser.add_argument('--process',     action='store', default='ttZ',                  choices=['ttZ','ttH','ttW'], help="Which process?")
-argParser.add_argument('--couplings',   action='store', default="cuW_0.01",   nargs='?', help="Give a underscore separated list of the non-zero couplings with their values, e.g. NAME1_VALUE1_NAME2_VALUE2")
-argParser.add_argument('--noGridpack',  action='store_true', help="Don't keep the gridpack")
-argParser.add_argument('--overwrite',   action='store_true', help="Overwrite exisiting x-sec calculation and gridpack")
-argParser.add_argument('--logLevel',    action='store', nargs='?', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'], default='INFO', help="Log level for logging" )
+argParser.add_argument('--process',     action='store',         default='ttZ',      choices=['ttZ','ttH','ttW'],     help="Which process?")
+argParser.add_argument('--model',       action='store',         default='HEL_UFO',  choices=['HEL_UFO', 'TopEffTh'], help="Which madgraph model?")
+argParser.add_argument('--couplings',   action='store',         default=[],         nargs='*',  type = str, help="Give a list of the non-zero couplings with values, e.g. NAME1 VALUE1 NAME2 VALUE2")
+argParser.add_argument('--noGridpack',  action='store_true',    help="Don't keep the gridpack")
+argParser.add_argument('--overwrite',   action='store_true',    help="Overwrite exisiting x-sec calculation and gridpack")
+argParser.add_argument('--logLevel',    action='store',         nargs='?', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG', 'TRACE', 'NOTSET'], default='INFO', help="Log level for logging" )
 
 args = argParser.parse_args()
 
 logger = logger.get_logger(args.logLevel, logFile = None)
 
-def checkTopEffTh(coups):
-    for c in coups:
-        if c[0] == "Lambda":
-            return 1
-    return 0
+# Check that we have an even number of arguments
+if not len(args.couplings)%2==0:
+    logger.error("Need an even number of coupling arguments of the format coupling1, value1, coupling2, value2, ... . Got %r", args.couplings )
 
-toList = args.couplings.split("_")
-activeCouplings = zip(toList[::2], map(float,toList[1::2]))
+# Interpret coupling argument list
+coupling_names  = args.couplings[::2]
+coupling_values = map(float,args.couplings[1::2])
 
-if args.model == 'TopEffTh':
-    if not checkTopEffTh(activeCouplings):
-        raise NotImplementedError("Cut-off scale not defined. Please set a value, e.g. Lambda_1000 .")
+# Check that we got Lambda in the TopEffTh model
+if args.model == 'TopEffTh' and "Lambda" not in coupling_names:
+    logger.warning( "Scale 'Lambda' not defined. Using Lambda = 1 TeV.")
+    coupling_names. append("Lambda")
+    coupling_values.append(1000)
 
-logger.info("Setting following coefficients: %s",activeCouplings)
+# zip for convinience 
+coupling_list = zip( coupling_names, coupling_values )
 
+# Let's not leave the user in the dark
+logger.info("Model:        %s", args.model)
+logger.info("Process:      %s", args.process)
+logger.info("Coefficients: %s", ", ".join( "%s=%5.4f" % c for c in coupling_list) )
+
+# Create configuration class
 config = configuration(args.model)
 config.setup()
 
 coup = couplings()
 if args.model == "HEL_UFO":
     coup.addBlock("newcoup", HEL_couplings_newcoup)
-    for c in activeCouplings:
+    for c in coupling_list:
         if not coup.setCoupling(c[0],c[1]):
             raise NotImplementedError("Coupling %s is not known"%c[0])
 
 elif args.model == "TopEffTh":
     coup.addBlock("dim6", TOP_EFT_couplings_dim6)
     coup.addBlock("fourfermion", TOP_EFT_couplings_fourfermion)
-    for c in activeCouplings:
+    for c in coupling_list:
         if not coup.setCoupling(c[0],c[1]):
             raise NotImplementedError("Coupling %s is not known"%c[0])
 
@@ -59,4 +67,3 @@ p.run(keepGridpack = not args.noGridpack, overwrite = args.overwrite)
 config.clean()
 
 logger.info("Done!")
-
