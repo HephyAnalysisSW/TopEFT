c5697044e426a54b84d0f4bc3175002df4828b00
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2017-08-28 17:57:14 +0200

    multi dimensional scan submission to batch

--- gencode/run2DOnHephyBatch.py
+++ gencode/run2DOnHephyBatch.py
@@ -3,22 +3,24 @@ import os
 import time
 import itertools
 
-# TopEFT
-from TopEFT.gencode.EFT import *
-
 # Logger
 import logging
 logger = logging.getLogger(__name__)
 
-nonZeroCouplings = ("RC3phiq", "RCtW")
-nonZeroCouplings = ("IC3phiq", "ICtW")
+#nonZeroCouplings = ("RC3phiq", "RCtW")
+#nonZeroCouplings = ("IC3phiq", "ICtW")
 
-n = 5
+nonZeroCouplings = ("DC1V","DC1A","DC2V","DC2A")
+
+nDim = len(nonZeroCouplings)
+
+n = 2
 
 # values
-couplingValues = [ i*10.0/n for i in range(-n,n+1) ]
+couplingValues = [ round(i*1.0/n,2) for i in range(-n,n+1) ]
+diag = [couplingValues]*nDim
 
-## this is how it should be done. However, becomes too expensive quite fast
+## this is how it should be done for 2D. However, becomes too expensive quite fast
 #couplingPairs = []
 #for a in itertools.permutations(cuW, len(cuB)):
 #    tmp = zip(a,cuB)
@@ -26,20 +28,24 @@ couplingValues = [ i*10.0/n for i in range(-n,n+1) ]
 #        if t not in couplingPairs: couplingPairs.append(t)#(round(t[0],2),round(t[1],2)))
 
 # this is the workaround
-couplingPairs = [a for a in itertools.permutations(couplingValues,2)] + zip(couplingValues,couplingValues)
-couplingPairs = [(round(a[0],2), round(a[1],2)) for a in couplingPairs]
+couplingGrid = [a for a in itertools.permutations(couplingValues,nDim)] + zip(*diag)
 
-#logger.info( len(couplingPairs) )
+allCombinations =  [ zip(a, nonZeroCouplings) for a in couplingGrid ]
+allCombinationsFlat = []
+for comb in allCombinations:
+    allCombinationsFlat.append([item for sublist in comb for item in list(sublist)])
 
 #processes = ['ttZ','ttW','ttH']
 processes = ['ttZ']
-#submitCMD = "submitBatch.py --title='2D'"
+#submitCMD = "submitBatch.py --title='DMmultDim'"
 submitCMD = "echo"
 
 for p in processes:
-    for c in couplingPairs:
-        couplingStr = "%s %s %s %s"%(nonZeroCouplings[0], c[0], nonZeroCouplings[1], c[1])
-        os.system(submitCMD+" 'python calcXSec.py --model TopEffTh --process "+p+" --couplings "+couplingStr+"'")
+    for comb in allCombinationsFlat:
+        strBase = "{} {} "*nDim
+        couplingStr = strBase.format(*comb)
+        #couplingStr = "%s %s %s %s"%(nonZeroCouplings[0], c[0], nonZeroCouplings[1], c[1])
+        os.system(submitCMD+" 'python calcXSec.py --model ewkDM --overwrite --process "+p+" --couplings "+couplingStr+"'")
         if not "echo" in submitCMD:
             time.sleep(60) # need to distribute load, shouldn't start with 40 jobs at a time
 
