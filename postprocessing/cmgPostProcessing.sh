d336347260795ab707b498a5f02a10e4b85e4c4c
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2017-09-27 17:41:19 +0200

    post processing

---
+++ postprocessing/cmgPostProcessing.sh
@@ -0,0 +1,11 @@
+#python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample TTZToLLNuNu_ext --noTopPtReweighting --overwrite # SPLIT20
+python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample WZTo3LNu_amcatnlo --noTopPtReweighting --overwrite # SPLIT10
+python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample TTWToLNu_ext --noTopPtReweighting --overwrite # SPLIT10
+python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample TTHnobb_pow --noTopPtReweighting --overwrite # SPLIT10
+python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample tWll --noTopPtReweighting --overwrite # SPLIT10
+python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample tZq_ll_ext --noTopPtReweighting --overwrite # SPLIT20
+#python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample  --noTopPtReweighting --overwrite # SPLIT20
+#python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample  --noTopPtReweighting --overwrite # SPLIT20
+#python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample  --noTopPtReweighting --overwrite # SPLIT20
+#python cmgPostProcessing.py --skim dilep --skipSystematicVariations --sample  --noTopPtReweighting --overwrite # SPLIT20
+
