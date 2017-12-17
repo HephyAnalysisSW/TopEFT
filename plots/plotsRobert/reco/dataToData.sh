4e1292b4cfbf5bc0ae2c6b0d019714c3314c44fd
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2017-12-17 09:11:36 +0100

    v18

--- plots/plotsRobert/reco/dataToData.sh
+++ plots/plotsRobert/reco/dataToData.sh
@@ -1,51 +1,51 @@
 #!/bin/sh
 
-python dataToData.py --selection dilepOS-njet3p-btag1p --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag1p --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag1p --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-btag0 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag0 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag0 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet0-btag0 --logLevel DEBUG
-python dataToData.py --selection dilepOS --logLevel DEBUG
+python dataToData.py --selection dilepOS-njet3p-btag1p
+python dataToData.py --selection dilepOS-njet2-btag1p
+python dataToData.py --selection dilepOS-njet1-btag1p
+python dataToData.py --selection dilepOS-njet3p-btag0
+python dataToData.py --selection dilepOS-njet3p
+python dataToData.py --selection dilepOS-njet2-btag0
+python dataToData.py --selection dilepOS-njet1-btag0
+python dataToData.py --selection dilepOS-njet0-btag0
+python dataToData.py --selection dilepOS
 
-python dataToData.py --selection dilepOS-njet3p-btag1p-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag1p-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag1p-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-btag0-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag0-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag0-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet0-btag0-onZ --logLevel DEBUG
-python dataToData.py --selection dilepOS-onZ --logLevel DEBUG
+python dataToData.py --selection dilepOS-njet3p-btag1p-onZ
+python dataToData.py --selection dilepOS-njet2-btag1p-onZ
+python dataToData.py --selection dilepOS-njet1-btag1p-onZ
+python dataToData.py --selection dilepOS-njet3p-btag0-onZ
+python dataToData.py --selection dilepOS-njet3p-onZ
+python dataToData.py --selection dilepOS-njet2-btag0-onZ
+python dataToData.py --selection dilepOS-njet1-btag0-onZ
+python dataToData.py --selection dilepOS-njet0-btag0-onZ
+python dataToData.py --selection dilepOS-onZ
 
-python dataToData.py --selection dilepOS-njet3p-btag1p-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag1p-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag1p-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-btag0-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag0-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag0-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet0-btag0-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-mll20 --logLevel DEBUG
+python dataToData.py --selection dilepOS-njet3p-btag1p-mll20
+python dataToData.py --selection dilepOS-njet2-btag1p-mll20
+python dataToData.py --selection dilepOS-njet1-btag1p-mll20
+python dataToData.py --selection dilepOS-njet3p-btag0-mll20
+python dataToData.py --selection dilepOS-njet3p-mll20
+python dataToData.py --selection dilepOS-njet2-btag0-mll20
+python dataToData.py --selection dilepOS-njet1-btag0-mll20
+python dataToData.py --selection dilepOS-njet0-btag0-mll20
+python dataToData.py --selection dilepOS-mll20
 
-python dataToData.py --selection dilepOS-njet3p-btag1p-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag1p-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag1p-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-btag0-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag0-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag0-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet0-btag0-mllTo20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-mllTo20 --logLevel DEBUG
+python dataToData.py --selection dilepOS-njet3p-btag1p-mllTo20
+python dataToData.py --selection dilepOS-njet2-btag1p-mllTo20
+python dataToData.py --selection dilepOS-njet1-btag1p-mllTo20
+python dataToData.py --selection dilepOS-njet3p-btag0-mllTo20
+python dataToData.py --selection dilepOS-njet3p-mllTo20
+python dataToData.py --selection dilepOS-njet2-btag0-mllTo20
+python dataToData.py --selection dilepOS-njet1-btag0-mllTo20
+python dataToData.py --selection dilepOS-njet0-btag0-mllTo20
+python dataToData.py --selection dilepOS-mllTo20
 
-python dataToData.py --selection dilepOS-njet3p-btag1p-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag1p-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag1p-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-btag0-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet3p-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet2-btag0-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet1-btag0-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-njet0-btag0-offZ-mll20 --logLevel DEBUG
-python dataToData.py --selection dilepOS-offZ-mll20 --logLevel DEBUG
+python dataToData.py --selection dilepOS-njet3p-btag1p-offZ-mll20
+python dataToData.py --selection dilepOS-njet2-btag1p-offZ-mll20
+python dataToData.py --selection dilepOS-njet1-btag1p-offZ-mll20
+python dataToData.py --selection dilepOS-njet3p-btag0-offZ-mll20
+python dataToData.py --selection dilepOS-njet3p-offZ-mll20
+python dataToData.py --selection dilepOS-njet2-btag0-offZ-mll20
+python dataToData.py --selection dilepOS-njet1-btag0-offZ-mll20
+python dataToData.py --selection dilepOS-njet0-btag0-offZ-mll20
+python dataToData.py --selection dilepOS-offZ-mll20
--- samples/python/cmgTuples_Data25ns_92X_Run2017_postProcessed.py
+++ samples/python/cmgTuples_Data25ns_92X_Run2017_postProcessed.py
@@ -12,7 +12,7 @@ except: from TopEFT.Tools.user import data_directory
 
 # Take post processing directory if defined in main module
 try:    postProcessing_directory = sys.modules['__main__'].postProcessing_directory
-except: postProcessing_directory = 'TopEFT_PP_2017_v14/dilep'
+except: postProcessing_directory = 'TopEFT_PP_2017_v18/dilep'
 
 logger.info("Loading data samples from directory %s", os.path.join(data_directory, postProcessing_directory))
 
@@ -28,14 +28,14 @@ for (run, version) in [('B',''),('C','')]:
     dirs["SingleElectron_Run2017"   + run + version ] = ["SingleElectron_"    + runTag + "_Trig_e"]
     dirs["SingleMuon_Run2017"       + run + version ] = ["SingleMuon_"        + runTag + "_Trig_mu"]
     dirs["SingleEleMu_Run2017"      + run + version ] = ["SingleMuon_"        + runTag + "_Trig_mu", "SingleElectron_"        + runTag + "_Trig_e_for_mu"]
-    dirs["MET_Run2017"       + run + version ]          = ["MET_"        + runTag]
+    dirs["MET_Run2017"              + run + version ] = ["MET_"               + runTag]
 
-for (run, version) in [('D',''),('E',''),('F','')]:
+for (run, version) in [('C','v2'), ('D',''),('E',''),('F','')]:
     runTag = 'Run2017' + run + version
     dirs["SingleElectron_Run2017"   + run + version ] = ["SingleElectron_"    + runTag + "_Trig_e"]
     dirs["SingleMuon_Run2017"       + run + version ] = ["SingleMuon_"        + runTag + "_Trig_mu"]
     dirs["SingleEleMu_Run2017"      + run + version ] = ["SingleMuon_"        + runTag + "_Trig_mu", "SingleElectron_"        + runTag + "_Trig_e_for_mu"]
-    dirs["MET_Run2017"       + run + version ]          = ["MET_"        + runTag]
+    dirs["MET_Run2017"              + run + version ] = ["MET_"               + runTag]
 
 
 def merge(pd, totalRunName, listOfRuns):
@@ -44,9 +44,11 @@ def merge(pd, totalRunName, listOfRuns):
 
 for pd in ['SingleElectron','SingleMuon','SingleEleMu','MET']:
     merge(pd, 'Run2017BC',      ['Run2017B', 'Run2017C'])
-    merge(pd, 'Run2017DEF',     ['Run2017D', 'Run2017E', 'Run2017F'])
-    merge(pd, 'Run2017BCD',     ['Run2017B', 'Run2017C', 'Run2017D'])
-    merge(pd, 'Run2017',        ['Run2017BCD', 'Run2017E', 'Run2017F'])
+    merge(pd, 'Run2017EF',      ['Run2017E', 'Run2017F'])
+    merge(pd, 'Run2017Cv2DEF',  ['Run2017Cv2','Run2017D', 'Run2017E', 'Run2017F'])
+    merge(pd, 'Run2017Cv2D',    ['Run2017Cv2','Run2017D'])
+    merge(pd, 'Run2017BCCv2D',  ['Run2017B', 'Run2017C', 'Run2017Cv2', 'Run2017D'])
+    merge(pd, 'Run2017',        ['Run2017BCCv2D', 'Run2017E', 'Run2017F'])
 
 for key in dirs:
     dirs[key] = [ os.path.join( data_directory, postProcessing_directory, dir) for dir in dirs[key]]
@@ -61,9 +63,17 @@ SingleElectron_Run2017BC       = getSample('SingleElectron',   'Run2017BC',     
 SingleMuon_Run2017BC           = getSample('SingleMuon',       'Run2017BC',       (1.)*1000)
 SingleEleMu_Run2017BC          = getSample('SingleEleMu',      'Run2017BC',       (1.)*1000)
 
-SingleElectron_Run2017DEF      = getSample('SingleElectron',   'Run2017DEF',       (1.)*1000)
-SingleMuon_Run2017DEF          = getSample('SingleMuon',       'Run2017DEF',       (1.)*1000)
-SingleEleMu_Run2017DEF         = getSample('SingleEleMu',      'Run2017DEF',       (1.)*1000)
+SingleElectron_Run2017Cv2D     = getSample('SingleElectron', 'Run2017Cv2D',       (1.)*1000)
+SingleMuon_Run2017Cv2D         = getSample('SingleMuon',     'Run2017Cv2D',       (1.)*1000)
+SingleEleMu_Run2017Cv2D        = getSample('SingleEleMu',    'Run2017Cv2D',       (1.)*1000)
+
+SingleElectron_Run2017Cv2DEF   = getSample('SingleElectron',   'Run2017Cv2DEF',       (1.)*1000)
+SingleMuon_Run2017Cv2DEF       = getSample('SingleMuon',       'Run2017Cv2DEF',       (1.)*1000)
+SingleEleMu_Run2017Cv2DEF      = getSample('SingleEleMu',      'Run2017Cv2DEF',       (1.)*1000)
+
+SingleElectron_Run2017EF       = getSample('SingleElectron',   'Run2017EF',       (1.)*1000)
+SingleMuon_Run2017EF           = getSample('SingleMuon',       'Run2017EF',       (1.)*1000)
+SingleEleMu_Run2017EF          = getSample('SingleEleMu',      'Run2017EF',       (1.)*1000)
 
 SingleElectron_Run2017         = getSample('SingleElectron',   'Run2017',       (1.)*1000)
 SingleMuon_Run2017             = getSample('SingleMuon',       'Run2017',       (1.)*1000)
