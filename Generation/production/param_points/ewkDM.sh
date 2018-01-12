f1153806e9b46f81975d33dbf17a8d61bd2d7226
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-01-12 14:02:57 +0100

    model

--- Generation/production/param_points/ewkDM.sh
+++ Generation/production/param_points/ewkDM.sh
@@ -14,16 +14,16 @@
 #run.py  --model ewkDM --process ttZ_ll --makeGridpack --calcXSec --couplings DC1A  1.0
 #run.py  --model ewkDM --process ttZ_ll --makeGridpack --calcXSec --couplings DC1V -1    DC1A 0.5
 
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.25
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG  0.25
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.25 
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG -0.25
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.5
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG  0.5
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.5 
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG -0.5
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.1767 DVG 0.1767
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.1767 DVG 0.1767
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.1767 DVG -0.1767
-run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.1767 DVG -0.1767
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.25 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG  0.25 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.25  &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG -0.25 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.5 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG  0.5 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.5  &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DVG -0.5 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.1767 DVG 0.1767 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.1767 DVG 0.1767 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG -0.1767 DVG -0.1767 &
+run.py  --model ewkDMGZ --process ttgamma_ll --makeGridpack --couplings  DAG  0.1767 DVG -0.1767 &
