8b0e23d01aabeee70692aae47a2fb35a80d1774a
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-01-13 12:40:09 +0100

    small

--- Generation/production/param_points/HEL_UFO.sh
+++ Generation/production/param_points/HEL_UFO.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-run.py --model HEL_UFO --couplings cuW -0.3 --calcXSec --makeGridpack --process ttZ_ll &
-run.py --model HEL_UFO --couplings cuW -0.2 --calcXSec --makeGridpack --process ttZ_ll &
-run.py --model HEL_UFO --couplings cuW -0.1 --calcXSec --makeGridpack --process ttZ_ll &
-run.py --model HEL_UFO --couplings cuW 0.0 --calcXSec --makeGridpack --process ttZ_ll &
-run.py --model HEL_UFO --couplings cuW 0.1 --calcXSec --makeGridpack --process ttZ_ll &
-run.py --model HEL_UFO --couplings cuW 0.2 --calcXSec --makeGridpack --process ttZ_ll &
-run.py --model HEL_UFO --couplings cuW 0.3 --calcXSec --makeGridpack --process ttZ_ll &
+run.py --model HEL_UFO --couplings cuW -0.3 --calcXSec --makeGridpack --process ttZ_ll 
+run.py --model HEL_UFO --couplings cuW -0.2 --calcXSec --makeGridpack --process ttZ_ll 
+run.py --model HEL_UFO --couplings cuW -0.1 --calcXSec --makeGridpack --process ttZ_ll 
+run.py --model HEL_UFO --couplings cuW 0.0 --calcXSec --makeGridpack --process ttZ_ll 
+run.py --model HEL_UFO --couplings cuW 0.1 --calcXSec --makeGridpack --process ttZ_ll 
+run.py --model HEL_UFO --couplings cuW 0.2 --calcXSec --makeGridpack --process ttZ_ll 
+run.py --model HEL_UFO --couplings cuW 0.3 --calcXSec --makeGridpack --process ttZ_ll 
