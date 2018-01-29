f4855da8ec26ae0a775c0ec1d82447cd8480f3c1
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-01-29 11:39:47 +0100

    remove getGenBoson.py

--- Tools/python/helpers.py
+++ Tools/python/helpers.py
@@ -310,3 +310,17 @@ def getPlotFromChain(c, var, binning, cutString = "(1)", weight = "weight", binn
         res.SetBinError(1 , sqrt(res.GetBinError(0)**2 + res.GetBinError(1)**2))
     return res
 
+def getGenZ(genparts):
+  for g in genparts:
+    if g['pdgId'] != 23:        continue					# pdgId == 23 for Z
+    if g['status'] != 62:	continue					# status 62 is last gencopy before it decays into ll/nunu
+    return g
+  return None
+
+def getGenPhoton(genparts):
+  for g in genparts:								# Type 0: no photon
+    if g['pdgId'] != 22:        continue					# pdgId == 22 for photons
+    if g['status'] != 23:	continue					# for photons, take status 23
+    return g
+  return None
+
