d5d10f2f66523354b28d907447e680f8879ab6c3
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-03-09 14:53:47 +0100

    recipe

--- setup94X.sh
+++ setup94X.sh
@@ -36,10 +36,11 @@ git checkout -b 94X_dev_StopsDilepton origin/94X_dev_StopsDilepton
 # https://github.com/CERN-PH-CMG/cmgtools-lite/pull/218
 git remote add cmssw-guitargeek https://github.com/guitargeek/cmssw.git -t ElectronID_MVA2017_940pre3 -f
 git format-patch --stdout 2efa972169e..64030f65aa2 | git apply -
-cd $CMSSW_BASE/external/slc6_amd64_gcc630
-git clone https://github.com/lsoffi/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
-cd data/RecoEgamma/ElectronIdentification/data
-git checkout CMSSW_9_4_0_pre3_TnP
+# Comment below because I added the training files under /src for running with crab
+#cd $CMSSW_BASE/external/slc6_amd64_gcc630
+#git clone https://github.com/lsoffi/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data
+#cd data/RecoEgamma/ElectronIdentification/data
+#git checkout CMSSW_9_4_0_pre3_TnP
 
 cd $CMSSW_BASE/src
 #compile
