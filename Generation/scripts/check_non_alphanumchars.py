11a3c4f3b7463dac828604a466ac9e55ddb58ae7
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-03-21 14:35:44 +0100

    special char fix

--- Generation/scripts/check_non_alphanumchars.py
+++ Generation/scripts/check_non_alphanumchars.py
@@ -1,11 +1,13 @@
+#!/usr/bin/env python
 import sys
 n=0
 if len(sys.argv)>1:
-    with open(sys.argv[1]) as f:
-        for i_line, line in enumerate(f.readlines()):
-            for i_char,char in enumerate(line):
-                n+=1
-                if ord(char) in [10]: continue #backspace
-                if ord(char)>ord('~') or ord(char)<ord(' '): print "file %s line %i char %i: ord %i %s" % (sys.argv[1], i_line, i_char, ord(char), char)
-    print "Checked %i characters in file %s" %( n, sys.argv[1] )
+    for filename in sys.argv[1:]:
+        with open(filename) as f:
+            for i_line, line in enumerate(f.readlines()):
+                for i_char,char in enumerate(line):
+                    n+=1
+                    if ord(char) in [10]: continue #backspace
+                    if ord(char)>ord('~') or ord(char)<ord(' '): print "file %s line %i char %i: ord %i %s" % (filename, i_line, i_char, ord(char), char)
+        print "Checked %i characters in file %s" %( n, filename )
 
