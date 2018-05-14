52f2f88d884b34625ede40db027b381443e2724f
Georg Moertl <georgmoertl@aon.at>
2018-05-14 11:20:58 +0200

    edit electronMVA stacked histogram

--- plots/plotsGeorg/stacked_plotexample1.py
+++ plots/plotsGeorg/stacked_plotexample1.py
@@ -8,24 +8,27 @@ from RootTools.core.standard import *
 
 # User specific 
 from TopEFT.Tools.user import plot_directory
+plot_directory_=os.path.join(plot_directory, 'stacked_plots')
+plot_directory=plot_directory_
+if not os.path.exists(plot_directory):
+    os.makedirs(plot_directory)
 
-# data 
-sample = Sample.fromFiles( "small", texName = "my first sample!", files = ["/afs/hephy.at/data/rschoefbeck01/cmgTuples/georg/TTJets_SingleLeptonFromTbar_1/treeProducer/tree.root"], treeName="tree")
+
+# data
+samplelist=[]
+#add samples 
+#sample = Sample.fromFiles( "small", texName = "my first sample!", files = ["/afs/hephy.at/data/rschoefbeck01/cmgTuples/georg/TTJets_SingleLeptonFromTbar_1/treeProducer/tree.root"], treeName="tree")
+samplelist.append(Sample.fromFiles( "small", texName = "QCD_Pt120to170", files = ["/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/QCD_Pt120to170/treeProducer/tree.root"], treeName="tree"))
+samplelist.append(Sample.fromFiles( "small", texName = "TTJets_SingleLeptonFromTbar", files = ["/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/TTJets_SingleLeptonFromTbar/treeProducer/tree.root"], treeName="tree"))
+#samplelist.append(Sample.fromFiles( "small", texName = "QCD_Pt120to170+TTJets_SingleLeptonFromTbar", files = ["/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/QCD_Pt120to170/treeProducer/tree.root","/afs/hephy.at/work/g/gmoertl/y.at/work/g/gmoertl/y.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/TTJets_SingleLeptonFromTbar/treeProducer/tree.root"], treeName="tree"))
 
 # variables to read
 
-variables = [ "run/I", "lumi/I", "evt/l", "lep_trackerHits/I", "lep_innerTrackChi2/F", "lep_pdgId/I", "lep_segmentCompatibility/F", "lep_sigmaIEtaIEta/F", "lep_etaSc/F", "lep_mcMatchPdgId/I" ]
-
-# variables for plots
-
-#varplot=["lep_trackerHits", "lep_innerTrackChi2", "lep_pdgId", "lep_segmentCompatibility", "lep_sigmaIEtaIEta", "lep_etaSc"]
-#varplotbins={"lep_trackerHits": 100, "lep_innerTrackChi2": 100, "lep_pdgId": 50, "lep_segmentCompatibility": 100, "lep_sigmaIEtaIEta": 100, "lep_etaSc": 100}
-#varplotmin={"lep_trackerHits": 0, "lep_innerTrackChi2": 0, "lep_pdgId": -25, "lep_segmentCompatibility": 0, "lep_sigmaIEtaIEta": 0, "lep_etaSc": 0}
-#varplotmax={"lep_trackerHits": 1, "lep_innerTrackChi2": 1, "lep_pdgId": 25, "lep_segmentCompatibility": 1, "lep_sigmaIEtaIEta": 1, "lep_etaSc": 1}
+variables = [ "run/I", "lumi/I", "evt/l", "lep_trackerHits/I", "lep_innerTrackChi2/F", "lep_pdgId/I", "lep_segmentCompatibility/F", "lep_sigmaIEtaIEta/F", "lep_etaSc/F", "lep_mcMatchPdgId/I", "lep_mcMatchId/I", "lep_mvaIdSpring16/F", "lep_pt/F" ]
 
 #define plots
 plots = [ 
-    {'name':'trackerHits', 'var':'lep_trackerHits', 'binning':[30,0,30]},
+    {'name':'electronMVA', 'var':'lep_mvaIdSpring16', 'binning':[100,0,1]},
     #{'name':'trackerHits', 'var':'lep_trackerHits', 'binning':[30,0,30]},
 ]
 
@@ -33,40 +36,48 @@ plots = [
 # declare histogram
 for p in plots:
     p['histo'] = ROOT.TH1F(p['var'], p['var'], *(p['binning']))
+    p['samplehistos']=[]
+    for sample in samplelist:
+        p['samplehistos'].append(ROOT.TH1F(p['var'], sample.texName, *(p['binning'])))
 
-# reader class
-reader = sample.treeReader(  map( TreeVariable.fromString, variables ) )
+i=0
+for sample in samplelist:
 
-# loop
-reader.start()
+    # reader class
+    reader = sample.treeReader(  map( TreeVariable.fromString, variables ) )
+    
+    # loop
+    reader.start()
 
+    PdgId=11
+    counter=0
+    while reader.run():
+        #print "run %i, lumi %i, event %i pdgId %i %6.5f" % (reader.event.run, reader.event.lumi, reader.event.evt, reader.event.lep_pdgId, reader.event.lep_segmentCompatibility) 
+        counter+=1
 
-PdgId=11
-counter=0
-while reader.run():
-    #print "run %i, lumi %i, event %i pdgId %i %6.5f" % (reader.event.run, reader.event.lumi, reader.event.evt, reader.event.lep_pdgId, reader.event.lep_segmentCompatibility) 
-    counter+=1
-
-    #reader_dict = {"lep_trackerHits/I": reader.event.lep_trackerHits,"lep_innerTrackChi2/F": reader.event.lep_innerTrackChi2,"lep_pdgId/I": reader.event.lep_pdgId,"lep_segmentCompatibility/F": reader.event.lep_segmentCompatibility,"lep_sigmaIEtaIEta/F": reader.event.lep_sigmaIEtaIEta,"lep_etaSc/F": reader.event.lep_etaSc}
-
-    for p in plots:
-       
-        #if abs(reader.event.lep_pdgId) == PdgId: hdict[str("h_"+var)].Fill(reader_dict[str(var)])
-        if abs(reader.event.lep_pdgId) == PdgId: p['histo'].Fill(getattr(reader.event, p['var']))
-
-        if counter>1000: break
-
-ptype='particle_type'
-if PdgId==11: ptype='electron'
-if PdgId==13: ptype='muon'
+        for p in plots:
+           if abs(reader.event.lep_pdgId) == PdgId:
+                p['histo'].Fill(getattr(reader.event, p['var']))
+                p['samplehistos'][i].Fill(getattr(reader.event, p['var']))
+           #if counter>1000: break
+    i+=1
 
 for p in plots:
     c=ROOT.TCanvas()
-    p['histo'].Draw('hist') 
+    hs=ROOT.THStack()
+    j=0
+    for histo in p['samplehistos']:
+        histo.SetFillColor(2+2*j)
+        histo.SetLineColor(1)
+        hs.Add(histo)
+        j+=1
+    hs.Draw('hist') 
+    hs.SetTitle(p['name'])
+    hs.GetXaxis().SetTitle(p['var'])
+    hs.GetYaxis().SetTitle("number of events")
     c.SetLogy()
-    c.SetLineColor(4)
-    p['histo'].SetFillColor(7)
-    c.Print(os.path.join(plot_directory, p['name']+'_plot.png'))
+    c.BuildLegend(0.05,0.75,0.3,0.95,"")
+    c.Print(os.path.join(plot_directory, p['name']+ '_stacked_plot.png'))
 
 
 
