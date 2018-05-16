d422aa68c98d7a0ed40cdab9a32992b1d88e3f45
Georg Moertl <georgmoertl@aon.at>
2018-05-16 14:12:04 +0200

    stacked samples

--- plots/plotsGeorg/stacked_plotexample3.py
+++ plots/plotsGeorg/stacked_plotexample3.py
@@ -68,165 +68,178 @@ read_variables = [
 mc    = [sample1,sample2]  # A full example would be e.g. mc = [ttbar, ttz, ttw, ...]
 stack = Stack(mc) # A full example would be e.g. stack = Stack( mc, [data], [signal1], [signal2] ) -> Samples in "mc" are stacked in the plot
 
-# Set some defaults -> these need not be specified below for each plot
-weight = lambda event, sample: 1.  # could be e.g. weight = lambda event, sample: event.weight
-selectionString = "(lep_pt>=25 && abs(lep_pdgId)==11 && lep_relIso03<0.1)"          # could be a complicated cut
-Plot.setDefaults(stack = stack, weight = weight, selectionString = selectionString, addOverFlowBin='upper')
+# Define some parameters for plots and selection
+ecaltypes=["","EndCap","Barrel"]
 
-# Sequence
-sequence = []
+for ecaltype in ecaltypes:
+    plotname="Ele"+ecaltype
 
-# Add a new fancy variable
-#def make_fancy_variable( event, sample ):
-#    event.fancy_variable = event.lep_trackerHits**2 
-#sequence.append( make_fancy_variable )
+    # Set some defaults -> these need not be specified below for each plot
+    weight = lambda event, sample: 1.  # could be e.g. weight = lambda event, sample: event.weight
+    selectionString = "(lep_pt>=25 && abs(lep_pdgId)==11 && lep_relIso03<0.1 && abs(lep_etaSc)<=1.479)" if ecaltype=="Barrel" else "(lep_pt>=25 && abs(lep_pdgId)==11 && lep_relIso03<0.1 && abs(lep_etaSc)>1.479)" if ecaltype=="EndCap" else "(lep_pt>=25 && abs(lep_pdgId)==11 && lep_relIso03<0.1)" # could be a complicated cut
+    Plot.setDefaults(stack = stack, weight = weight, selectionString = selectionString, addOverFlowBin='upper')
 
+    # Sequence
+    sequence = []
 
-def isPrompt(lepton):
-    return abs(lepton.lep_mcMatchId) in [6,23,24,25,37]
-
-def isNonPrompt(lepton):
-    return not isPrompt(lepton) and abs(lepton.lep_mcMatchAny) in [4, 5]
-
-def isFake(lepton):
-    return not isPrompt(lepton) and not ( abs(lepton.lep_mcMatchAny) in [4, 5] )
-
-def print_mcmatchId( event, sample ):
-    if isNonPrompt(event) and event.lep_mvaIdSpring16<0.3 and sample.name=="TTJets":
-        print event.lep_mcMatchId
-
-#sequence.append(print_mcmatchId)
+    # Add a new fancy variable
+    def make_absEInvMinusPInv( event, sample ):
+        event.absEInvMinusPInv = abs(event.lep_eInvMinusPInv)
+    sequence.append( make_absEInvMinusPInv)
 
 
-# Start with an empty list
-plots = []
+    def isPrompt(lepton):
+        return abs(lepton.lep_mcMatchId) in [6,23,24,25,37]
 
-# Add plots
-plots.append(Plot(name='EleMVA',
-    texX = 'electron MVA', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_mvaIdSpring16/F" ),
-    binning=[30,-1,1],
-))
-plots.append(Plot( name='EleMVAclassPrompt',
-    texX = 'electron MVA', texY = 'Number of Events',
-    attribute = lambda lepton, sample: lepton.lep_mvaIdSpring16 if isPrompt(lepton) else float('nan'),
-    binning=[30,-1,1],
-))
-plots.append(Plot( name='EleMVAclassNonPrompt',
-    texX = 'electron MVA', texY = 'Number of Events',
-    attribute = lambda lepton, sample: lepton.lep_mvaIdSpring16 if isNonPrompt(lepton) else float('nan'),
-    binning=[30,-1,1],
-))
-plots.append(Plot( name='EleMVAclassFake',
-    texX = 'electron MVA', texY = 'Number of Events',
-    attribute = lambda lepton, sample: lepton.lep_mvaIdSpring16 if isFake(lepton) else float('nan'),
-    binning=[30,-1,1],
-))
+    def isNonPrompt(lepton):
+        return not isPrompt(lepton) and abs(lepton.lep_mcMatchAny) in [4, 5]
 
-plots.append(Plot(name='ElePt',
-    texX = 'pt', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_pt/F" ),
-    binning=[100,0,500],
-))
-plots.append(Plot(name='pdgId',
-    texX = 'pdgId', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_pdgId/I" ),
-    binning=[61,-30,30],
-))
-plots.append(Plot(name='EleEtaSc',
-    texX = 'etaSc', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_etaSc/F" ),
-    binning=[60,-3,3],
-))
-plots.append(Plot(name='EleFull5x5SigmaIetaIeta',
-    texX = 'full5x5_sigmaIetaIeta', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_full5x5_sigmaIetaIeta/F" ),
-    binning=[100,0,0.1],
-))
-plots.append(Plot(name='EleDEtaInSeed',
-    texX = 'dEtaInSeed', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_dEtaInSeed/F" ),
-    binning=[100,-4,4],
-))
-plots.append(Plot(name='EleDPhiScTrkIn',
-    texX = 'dPhiScTrkIn', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_dPhiScTrkIn/F" ),
-    binning=[100,-2,2],
-))
-plots.append(Plot(name='EleRelIso03',
-    texX = 'relIso03', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_relIso03/F" ),
-    binning=[100,0,0.11],
-))
-plots.append(Plot(name='EleEInvMinusPInv',
-    texX = 'eInvMinusPInv', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_eInvMinusPInv/F" ),
-    binning=[60,-1,1],
-))
-plots.append(Plot(name='EleLostOuterHits',
-    texX = 'lostOuterHits', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_lostOuterHits/I" ),
-    binning=[16,0,15],
-))
-plots.append(Plot(name='EleConvVeto',
-    texX = 'convVeto', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_convVeto/I" ),
-    binning=[2,0,1],
-))
-plots.append(Plot(name='EleHadronicOverEm',
-    texX = 'hadronicOverEm', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_hadronicOverEm/F" ),
-    binning=[30,0,3],
-))
-plots.append(Plot(name='EleRho',
-    texX = 'rho', texY = 'Number of Events',
-    attribute = TreeVariable.fromString( "lep_rho/F" ),
-    binning=[100,0,5],
-))
+    def isFake(lepton):
+        return not isPrompt(lepton) and not ( abs(lepton.lep_mcMatchAny) in [4, 5] )
 
+    def print_mcmatchId( event, sample ):
+        if isNonPrompt(event) and event.lep_mvaIdSpring16<0.3 and sample.name=="TTJets":
+            print event.lep_mcMatchId
 
-#plots.append(Plot( name = "fancy_variable",
-#    texX = 'Number of tracker hits squared', texY = 'Number of Events',
-#    attribute = lambda event, sample: event.fancy_variable, # <--- can use any 'event' attribute, including the ones we define in 'sequence'    binning=[30,0,900],
-#))
+    #sequence.append(print_mcmatchId)
 
+    # Start with an empty list
+    plots = []
+    # Add plots
+    plots.append(Plot(name=plotname+'MVA',
+        texX = 'electron MVA', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_mvaIdSpring16/F" ),
+        binning=[30,-1,1],
+    ))
+    plots.append(Plot( name=plotname+'MVAclassPrompt',
+        texX = 'electron MVA', texY = 'Number of Events',
+        attribute = lambda lepton, sample: lepton.lep_mvaIdSpring16 if isPrompt(lepton) else float('nan'),
+        binning=[30,-1,1],
+    ))
+    plots.append(Plot( name=plotname+'MVAclassNonPrompt',
+        texX = 'electron MVA', texY = 'Number of Events',
+        attribute = lambda lepton, sample: lepton.lep_mvaIdSpring16 if isNonPrompt(lepton) else float('nan'),
+        binning=[30,-1,1],
+    ))
+    plots.append(Plot( name=plotname+'MVAclassFake',
+        texX = 'electron MVA', texY = 'Number of Events',
+        attribute = lambda lepton, sample: lepton.lep_mvaIdSpring16 if isFake(lepton) else float('nan'),
+        binning=[30,-1,1],
+    ))
 
-# Fill everything.
-plotting.fill(plots, read_variables = read_variables, sequence = sequence)
+    plots.append(Plot(name=plotname+'Pt',
+        texX = 'pt', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_pt/F" ),
+        binning=[100,0,500],
+    ))
+    plots.append(Plot(name=plotname+'pdgId',
+        texX = 'pdgId', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_pdgId/I" ),
+        binning=[61,-30,30],
+    ))
+    plots.append(Plot(name=plotname+'EtaSc',
+        texX = 'etaSc', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_etaSc/F" ),
+        binning=[60,-3,3],
+    ))
+    plots.append(Plot(name=plotname+'Full5x5SigmaIetaIeta',
+        texX = 'full5x5_sigmaIetaIeta', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_full5x5_sigmaIetaIeta/F" ),
+        binning=[30,0,0.06],
+    ))
+    plots.append(Plot(name=plotname+'DEtaInSeed',
+        texX = 'dEtaInSeed', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_dEtaInSeed/F" ),
+        binning=[30,-0.03,0.03],
+    ))
+    plots.append(Plot(name=plotname+'DPhiScTrkIn',
+        texX = 'dPhiScTrkIn', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_dPhiScTrkIn/F" ),
+        binning=[20,-0.2,0.2],
+    ))
+    plots.append(Plot(name=plotname+'RelIso03',
+        texX = 'relIso03', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_relIso03/F" ),
+        binning=[90,0,0.3],
+    ))
+    plots.append(Plot(name=plotname+'EInvMinusPInv',
+        texX = '|1/E-1/p|', texY = 'Number of Events',
+        attribute = lambda event, sample: event.absEInvMinusPInv,
+        binning=[30,0,0.15],
+    ))
+    plots.append(Plot(name=plotname+'LostOuterHits',
+        texX = 'lostOuterHits', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_lostOuterHits/I" ),
+        binning=[16,0,15],
+    ))
+    plots.append(Plot(name=plotname+'ConvVeto',
+        texX = 'convVeto', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_convVeto/I" ),
+        binning=[2,0,1],
+    ))
+    plots.append(Plot(name=plotname+'HadronicOverEm',
+        texX = 'hadronicOverEm', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_hadronicOverEm/F" ),
+        binning=[30,0,0.1],
+    ))
+    plots.append(Plot(name=plotname+'Rho',
+        texX = 'rho', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_rho/F" ),
+        binning=[100,0,5],
+    ))
+    plots.append(Plot(name=plotname+'Dxy',
+        texX = 'dxy', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_dxy/F" ),
+        binning=[60,-0.1,0.1],
+    ))
+    plots.append(Plot(name=plotname+'Dz',
+        texX = 'dz', texY = 'Number of Events',
+        attribute = TreeVariable.fromString( "lep_dz/F" ),
+        binning=[60,-0.2,0.2],
+    ))
 
-#
-# Helper: Add text on the plots
-#
-def drawObjects( plotData, dataMCScale, lumi_scale ):
-    tex = ROOT.TLatex()
-    tex.SetNDC()
-    tex.SetTextSize(0.04)
-    tex.SetTextAlign(11) # align right
-    lines = [
-      (0.15, 0.95, 'CMS Preliminary' if plotData else 'CMS Simulation'),
-      (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV) Scale %3.2f'% ( lumi_scale, dataMCScale ) ) if plotData else (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV)' % lumi_scale)
-    ]
-    return [tex.DrawLatex(*l) for l in lines]
-
-# Draw a plot and make it look nice-ish
-def drawPlots(plots, dataMCScale):
-  for log in [False, True]:
-    plot_directory_ = os.path.join(plot_directory, 'histogram_plots', 'stacked_samples', ("log" if log else "lin"))
-    for plot in plots:
-      if not max(l[0].GetMaximum() for l in plot.histos): continue # Empty plot
-      
-      plotting.draw(plot,
-        plot_directory = plot_directory_,
-        #ratio = {'yRange':(0.1,1.9)} if not args.noData else None,
-        logX = False, logY = log, sorting = True,
-        yRange = (0.03, "auto") if log else (0.001, "auto"),
-        scaling = {},
-        legend = [ (0.15,0.9-0.03*sum(map(len, plot.histos)),0.9,0.9), 2],
-        drawObjects = drawObjects( False, dataMCScale , lumi_scale = -1 ),
-        copyIndexPHP = True
-      )
+    #plots.append(Plot( name = "fancy_variable",
+    #    texX = 'Number of tracker hits squared', texY = 'Number of Events',
+    #    attribute = lambda event, sample: event.fancy_variable, # <--- can use any 'event' attribute, including the ones we define in 'sequence'    binning=[30,0,900],
+    #))
 
 
-# Draw the plots
-drawPlots(plots, dataMCScale = -1)
+    # Fill everything.
+    plotting.fill(plots, read_variables = read_variables, sequence = sequence)
+
+    #
+    # Helper: Add text on the plots
+    #
+    def drawObjects( plotData, dataMCScale, lumi_scale ):
+        tex = ROOT.TLatex()
+        tex.SetNDC()
+        tex.SetTextSize(0.04)
+        tex.SetTextAlign(11) # align right
+        lines = [
+          (0.15, 0.95, 'CMS Preliminary' if plotData else 'CMS Simulation'),
+          (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV) Scale %3.2f'% ( lumi_scale, dataMCScale ) ) if plotData else (0.45, 0.95, 'L=%3.1f fb{}^{-1} (13 TeV)' % lumi_scale)
+        ]
+        return [tex.DrawLatex(*l) for l in lines]
+
+    # Draw a plot and make it look nice-ish
+    def drawPlots(plots, dataMCScale):
+      for log in [False, True]:
+        plot_directory_ = os.path.join(plot_directory, 'histogram_plots', 'stacked_samples', plotname, ("log" if log else "lin"))
+        for plot in plots:
+          if not max(l[0].GetMaximum() for l in plot.histos): continue # Empty plot
+          
+          plotting.draw(plot,
+            plot_directory = plot_directory_,
+            #ratio = {'yRange':(0.1,1.9)} if not args.noData else None,
+            logX = False, logY = log, sorting = True,
+            yRange = (0.03, "auto") if log else (0.001, "auto"),
+            scaling = {},
+            legend = [ (0.15,0.9-0.03*sum(map(len, plot.histos)),0.9,0.9), 2],
+            drawObjects = drawObjects( False, dataMCScale , lumi_scale = -1 ),
+            copyIndexPHP = True
+          )
+
+
+    # Draw the plots
+    drawPlots(plots, dataMCScale = -1)
 
