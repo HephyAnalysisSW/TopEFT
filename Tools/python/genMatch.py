85f19ca2c8a6e6838f0a6aeaee3396009cb58663
Daniel Spitzbart <daniel.spitzbart@cern.ch>
2018-05-25 17:12:33 +0200

    minor

--- Tools/python/genMatch.py
+++ Tools/python/genMatch.py
@@ -2,8 +2,9 @@
 def getGenMatch(lepton, genParticles):
 
     # Defaults
-    match = None
-    minDeltaR = 9999.
+    match       = None
+    minDeltaR   = 9999.
+    isPrompt    = False
 
     # Filter
     genParticles = [ p for p in genParticles if (abs(p['pdgId'])==abs(lepton['pdgId']) or p['pdgId']==22)  ]
