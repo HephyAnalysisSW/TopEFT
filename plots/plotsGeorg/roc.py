7b0d167cb08b5b12bf0f5fb4ce3161c3de461b9e
Georg Moertl <georgmoertl@aon.at>
2018-05-28 16:13:26 +0200

    edit stacked classes

--- plots/plotsGeorg/roc.py
+++ plots/plotsGeorg/roc.py
@@ -21,15 +21,16 @@ samplelist=[]
 #], treeName="tree"))
 
 #TTJets files
-samplelist.append(Sample.fromFiles( "TTJets", texName = "TTJets_SingleLeptonFromTbar", files = [
-"/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/TTJets_SingleLeptonFromTbar/treeProducer/tree.root"
-], treeName="tree"))
-
-#QCD + TTJets
-#samplelist.append(Sample.fromFiles( "QCD+TTJets", texName = "QCD_Pt120to170+TTJets_SingleLeptonFromTbar", files = [
-#"/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/QCD_Pt120to170/treeProducer/tree.root",
+#samplelist.append(Sample.fromFiles( "TTJets", texName = "TTJets_SingleLeptonFromTbar", files = [
 #"/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/TTJets_SingleLeptonFromTbar/treeProducer/tree.root"
 #], treeName="tree"))
+
+#QCD + TTJets
+samplelist.append(Sample.fromFiles( "QCD+TTJets", texName = "QCD_Pt120to170+TTJets_SingleLeptonFromTbar", files = [
+"/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/QCD_Pt120to170/treeProducer/tree.root",
+"/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/TTJets_SingleLeptonFromTbar/treeProducer/tree.root",
+"/afs/hephy.at/work/g/gmoertl/CMSSW_9_4_6_patch1/src/CMSData/TTJets_SingleLeptonFromTbar_1/treeProducer/tree.root"
+], treeName="tree"))
 
 # variables to read
 variables = [
