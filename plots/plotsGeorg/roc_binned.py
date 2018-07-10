4332aa4b5427395bdb28ffa8fe70b1b076d30f61
Georg Moertl <georgmoertl@aon.at>
2018-07-10 11:49:17 +0200

    edit roc curves

--- plots/plotsGeorg/roc_binned.py
+++ plots/plotsGeorg/roc_binned.py
@@ -3,6 +3,7 @@ import ROOT
 import os
 from array import array
 from copy import deepcopy
+import math
 
 # RootTools
 from RootTools.core.standard import *
@@ -11,7 +12,6 @@ from RootTools.core.standard import *
 from TopEFT.Tools.user import plot_directory
 plot_directory_=os.path.join(plot_directory, 'DeepLepton')
 plot_directory=plot_directory_
-
 
 #define mixed QCD + TTJets samples for electorns and muons
 PlotTrainData=1  #1=true, 0=false
@@ -107,7 +107,7 @@ variables = [
 #"lep_full5x5_sigmaIetaIeta/F",  #electrons (barrel: <0.0104,     endcap: <0.0305)                               #Electron full5x5_sigmaIetaIeta for Leptons after the preselection : 0 at: 0x411d5c0 
 #"lep_dEtaInSeed/F",             #electrons (barrel: | |<0.00353, endcap: | |<0.00567)                           #DeltaEta wrt ele SC seed for Leptons after the preselection : 0 at: 0x411e850
 #"lep_dPhiScTrkIn/F",            #electrons (barrel: | |<0.0499,  endcap: | |<0.0165)                            #Electron deltaPhiSuperClusterTrackAtVtx (without absolute value!) for Leptons after the preselection : 0 at: 0x41083f0
-"lep_relIso03/F",               #electrons (barrel: <0.0361,     endcap: <0.094)       #muons <0.1              #PF Rel Iso, R=0.3, pile-up corrected for Leptons after the preselection : 0 at: 0x410f640
+#"lep_relIso03/F",               #electrons (barrel: <0.0361,     endcap: <0.094)       #muons <0.1              #PF Rel Iso, R=0.3, pile-up corrected for Leptons after the preselection : 0 at: 0x410f640
 #"lep_eInvMinusPInv/F",          #electrons (barrel: | |<0.0278,  endcap: | |<0.0158)                            #Electron 1/E - 1/p  (without absolute value!) for Leptons after the preselection : 0 at: 0x4108f90   
 #"lep_lostOuterHits/I",          #electrons (barrel: 1,           endcap: 1)                                     #Number of lost hits on inner track for Leptons after the preselection : 0 at: 0x4101ee0
 #"lep_convVeto/I",               #electrons yes                                                                  #Conversion veto (always true for muons) for Leptons after the preselection : 0 at: 0x410e5c0
@@ -126,22 +126,17 @@ variables = [
 ]
 
 
-
 #define plot structure
 leptonFlavourList=[]
 #leptonFlavourList.append({"Name":"Electron", "ShortName":"ele", "pdgId":11, "sample":sampleEle})
 leptonFlavourList.append({"Name":"Muon", "ShortName":"muo", "pdgId":13, "sample":sampleMuo})
 
 MVAList=[]
-MVAList.append({"Name":"MVA_Fall17noIso", "Var":"lep_mvaIdFall17noIso", "plotColor":[ROOT.kGray,ROOT.kGray+1,ROOT.kGray+2,ROOT.kGray+3,ROOT.kGray+4], "lineWidth":1})
-MVAList.append({"Name":"DeepLeptonSummer18", "Var":"prob_lep_isPromptId", "plotColor":[ROOT.kRed,ROOT.kRed+1,ROOT.kRed+2,ROOT.kRed+3,ROOT.kRed+4], "lineWidth":2})
+MVAList.append({"Name":"MVA_Fall17noIso", "Var":"lep_mvaIdFall17noIso", "plotColor":ROOT.kGray, "lineWidth":1})
+MVAList.append({"Name":"DeepLeptonSummer18", "Var":"prob_lep_isPromptId", "plotColor":ROOT.kRed, "lineWidth":2})
 
-pt_cuts=[]
-pt_cuts.append({"Name":"pt15to25","lower_limit":15, "upper_limit":25})
-pt_cuts.append({"Name":"pt>25","lower_limit":25, "upper_limit":1000})
-
-cutList=[]
-cutList.append({"Name":"relIso", "VarName":"relIso03", "Var":"lep_relIso03", "cuts":[0.1,0.2,0.3,0.4,0.5], "operator":"<="})
+binnedList=[]
+binnedList.append({"Name":"pt", "VarName":"|pt|", "Var":"lep_pt", "abs":1, "cuts":[0, 250], "bins":50})
 
 #define function to calculate eS and eB
 def eS(p, rocdataset):
@@ -164,90 +159,85 @@ def eB(p, rocdataset):
                 ntruthid+=1.
     return 0. if ntruth==0. else ntruthid/ntruth
 
-#loop over Leptons, MVAs and Cuts
 
 for leptonFlavour in leptonFlavourList:
     if leptonFlavour["Name"]=="Muon":
         MVAList=[]
-        MVAList.append({"Name":"DeepLeptonSummer18", "Var":"prob_lep_isPromptId", "plotColor":[ROOT.kRed,ROOT.kRed+1,ROOT.kRed+2,ROOT.kRed+3,ROOT.kRed+4], "lineWidth":2})
-
-    for pt_cut in pt_cuts:
+        MVAList.append({"Name":"DeepLeptonSummer18", "Var":"prob_lep_isPromptId", "plotColor":ROOT.kRed, "lineWidth":2})
     
-        for var in cutList:
-            readerdata=[]
-            plotLegend=[]
+    for binVar in binnedList:
+
+        colorList=[]
+        lineWidthList=[]
+        
+        #Initialize Mulitgraph
+        c=ROOT.TCanvas()
+        c.SetLogy()
+        mg=ROOT.TMultiGraph()
+        g=[]
+        ng=0
+
+        for MVA in MVAList:
             
-            colorList=[]
-            lineWidthList=[]
-            
-            #Draw TGraph
-            c=ROOT.TCanvas()
-            if logY: c.SetLogy()
-            mg=ROOT.TMultiGraph()
-            g=[]
-            ng=0
-             
-            for MVA in MVAList:
-                i=0
-                for cut in var["cuts"]:
-                    legendString=MVA["Name"]+" |pdgId|="+str(leptonFlavour["pdgId"])+", "+var["VarName"]+var["operator"]+str(cut)
-                    plotLegend.append(legendString)
-                    colorList.append(MVA["plotColor"][i])
-                    lineWidthList.append(MVA["lineWidth"])
-                    readerdata.append([])
-                    i += 1
+            colorList.append(MVA["plotColor"])
+            lineWidthList.append(MVA["lineWidth"])
 
             # reader class
+            readerData=[[] for i in xrange(binVar["bins"])]
             reader = leptonFlavour["sample"].treeReader(  map( TreeVariable.fromString, variables ) )
-            # loop
             reader.start()
-
             while reader.run():
-                j=0
-                for MVA in MVAList:
-                    for cut in var["cuts"]:
-                        if abs(reader.event.lep_pdgId)==leptonFlavour["pdgId"] and (reader.event.lep_pt>=pt_cut["lower_limit"] and reader.event.lep_pt<=pt_cut["upper_limit"]):
-                            if (getattr(reader.event,var["Var"]) <= cut if var["operator"]=="<=" else getattr(reader.event,var["Var"]) >= cut):
-                                readerdata[j].append([reader.event.lep_isPromptId, getattr(reader.event, MVA["Var"])])            
-                                #print "pdgId %i, pt %f" %(reader.event.lep_pdgId, reader.event.lep_pt)
-                        j += 1
-
-            #calculate eS and eB
-            for dataset in readerdata:
-                p=array('d')
-                x=array('d')
-                y=array('d')
-
-                prange=[pval*0.01 for pval in xrange(-100,100)]
+                if abs(reader.event.lep_pdgId)==leptonFlavour["pdgId"]:
+                    cut_val = getattr(reader.event, binVar["Var"]) 
+                    if cut_val >= binVar["cuts"][0] and cut_val <  binVar["cuts"][1]:
+                        j=int(math.ceil(cut_val/(binVar["cuts"][1]-binVar["cuts"][0])*binVar["bins"]))-1
+                        readerData[j].append([reader.event.lep_isPromptId, getattr(reader.event, MVA["Var"])])
+                        #print "j=%i, pt=%f" %(j, reader.event.lep_pt)
+                        #print "i=%i,j=%i,  pt=%f" %(i,j, reader.event.lep_pt)
+            
+            j=0
+            x=array('d')
+            y=array('d')
+            for dataset in readerData:
+                #print j+1, len(dataset)
+                j += 1
+                prange=[pval*0.001 for pval in range(950,1000)]
                 for pval in prange:
-                    x.append(eS(pval, dataset))
-                    y.append(eB(pval, dataset)) if logY else y.append(1-eB(pval, dataset))
+                    xval=eB(pval,dataset)
+                    p=pval 
+                    #print pval, xval
+                    if xval<=0.01:
+                        break
+                if not len(dataset)==0:
+                    x.append(j*5)
+                    y.append(eS(p,dataset))
 
-                gname=plotLegend[ng]
-                n=len(x)
-                g.append(ROOT.TGraph(n,x,y))
-                g[ng].SetName(gname)
-                g[ng].SetTitle(gname)
-                g[ng].SetLineColor(colorList[ng])
-                g[ng].SetLineWidth(lineWidthList[ng])
-                g[ng].SetMarkerColor(colorList[ng])
-                #g[ng].SetMarkerStyle( 5 )
-                g[ng].SetFillStyle(0)
-                g[ng].SetFillColor(0)
-                g[ng].SetMarkerSize(0)
-                g[ng].Draw("C")
-                #nmaxtext.DrawLatex(x[nmax],y[nmax],"mvaId=%1.2f" %p[nmax])
-                mg.Add(g[ng])
-                ng += 1
-            mg.Draw("AC")
-            mg.SetTitle(pt_cut["Name"]+" "+leptonFlavour["sample"].texName+(" - TrainData" if PlotTrainData else " - TestData"))
-            mg.GetXaxis().SetTitle('eS')
-            mg.GetXaxis().SetLimits(0.597, 1.003)
-            mg.GetYaxis().SetTitle('eB' if logY else '1-eB')
-            mg.GetYaxis().SetRangeUser(0.0009, 1.01) if logY else mg.GetYaxis().SetLimits(0.0, 1.0)
-            #c.BuildLegend(0.12,0.90,0.5,0.7) if logY else c.BuildLegend()
-            c.BuildLegend()
-            directory=(os.path.join(plot_directory, ElectronDate if leptonFlavour["Name"]=="Electron" else MuonDate, leptonFlavour["Name"]))
-            if not os.path.exists(directory):
-                os.makedirs(directory)
-            c.Print(os.path.join(directory, 'roc_'+pt_cut["Name"]+'_cut_'+var["Name"]+'.png'))
+            #Draw Graphs
+            n=len(x)
+            g.append(ROOT.TGraph(n,x,y))
+            gname=(MVA["Name"]+": "+binVar["Var"]+", "+"eB<=0.01")
+            g[ng].SetName(gname)
+            g[ng].SetTitle(gname)
+            g[ng].SetLineColor(colorList[ng])
+            g[ng].SetLineWidth(lineWidthList[ng])
+            g[ng].SetMarkerColor(colorList[ng])
+            #g[ng].SetMarkerStyle( 5 )
+            g[ng].SetFillStyle(0)
+            g[ng].SetFillColor(0)
+            g[ng].SetMarkerSize(0)
+            g[ng].Draw("C")
+            #nmaxtext.DrawLatex(x[nmax],y[nmax],"mvaId=%1.2f" %p[nmax])
+            mg.Add(g[ng])
+            ng += 1
+
+        #Draw Multigraph
+        mg.Draw("AC")
+        mg.SetTitle(leptonFlavour["sample"].texName+(" - TrainData" if PlotTrainData else " - TestData"))
+        mg.GetXaxis().SetTitle('pt')
+        mg.GetYaxis().SetTitle('eS')
+        #mg.GetYaxis().SetRangeUser(0.0,1.0)
+        c.BuildLegend()
+        directory=(os.path.join(plot_directory, ElectronDate if leptonFlavour["Name"]=="Electron" else MuonDate, leptonFlavour["Name"]))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        c.Print(os.path.join(directory, 'roc_binned_'+binVar["Name"]+'.png'))
