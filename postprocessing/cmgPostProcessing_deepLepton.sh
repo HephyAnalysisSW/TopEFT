0376b9ecb77c18400eeabe775fec43cc34827930
Georg Moertl <georgmoertl@aon.at>
2018-11-22 19:55:29 +0100

    mc postprocessing

--- postprocessing/cmgPostProcessing_deepLepton.sh
+++ postprocessing/cmgPostProcessing_deepLepton.sh
@@ -7,8 +7,8 @@
 ### Summer 16 MC ###
 
 ##TTJets
-#python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTJets_SingleLeptonFromTbar TTJets_SingleLeptonFromTbar_ext #SPLIT200  
-#python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTJets_SingleLeptonFromT TTJets_SingleLeptonFromT_ext #SPLIT200
+python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTJets_SingleLeptonFromTbar TTJets_SingleLeptonFromTbar_ext #SPLIT200  
+python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTJets_SingleLeptonFromT TTJets_SingleLeptonFromT_ext #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTJets_DiLepton TTJets_DiLepton_ext #SPLIT135
 ##python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTLep_pow #SPLIT50
 ##python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample TTSemiLep_pow #SPLIT50
@@ -48,14 +48,14 @@
 #### Data 16 ###
 #
 ##Double Muon
-python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016B_07Aug17_v2 #SPLIT200
+#python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016B_07Aug17_v2 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016C_07Aug17 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016D_07Aug17 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016E_07Aug17 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016F_07Aug17 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016G_07Aug17 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  DoubleMuon_Run2016H_07Aug17 #SPLIT200
-#
+
 ##Single Muon
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  SingleMuon_Run2016B_07Aug17_v2 #SPLIT200
 #python cmgPostProcessing.py --deepLepton --skim singlelep --forceProxy --fileBasedSplitting --overwrite --remakeTTVLeptonMVA --year 2016 --processingEra deepLepton_v4 --sample  SingleMuon_Run2016C_07Aug17 #SPLIT200
